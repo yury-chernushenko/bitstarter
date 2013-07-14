74d4988728be3f317db6bfe84fa677d463973031
Yury Chernushenko <y.chernushenko@gmail.com>
2013-07-14 07:45:29 +0000

    web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var infile = "index.html";
+
+var content = fs.readFile();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(content.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
